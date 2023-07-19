5fd709f2f58bb8d8dd4b5cbb2dc9aa031e347dbc
tjdwns4537 <tjdwns4537@naver.com>
2023-07-19 17:33:50 +0900

    apply gitignore

--- app/src/main/java/dictionary/dictionaryandroid/helper/NaverAPI.java
+++
@@ -1,6 +0,0 @@
-package dictionary.dictionaryandroid.helper;
-
-public class NaverAPI {
-    private static String ClientID = "VutVA2sKPIpgyGnhXVXm";
-    private static String ClientSecretKey = "ciCIWQz6BZ";
-}
